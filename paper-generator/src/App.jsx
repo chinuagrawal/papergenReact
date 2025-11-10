bb6b159f541793de660c7a390c7bbbfdd6c0cf5a
Chinmay Agrawal <thechinmayagrawal@gmail.com>
2025-11-10 15:21:09 +0530

    created react and instal dom for routes

---
+++ paper-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React 👋</h1>
+      <p>My first React app is running!</p>
+    </div>
+  );
+}
+
+export default App;
+
