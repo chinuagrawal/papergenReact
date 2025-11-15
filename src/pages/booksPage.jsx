ef63737a85b2e8b77ca6ad6b9ab6ec4a2b2eb0d6
Chinmay Agrawal <thechinmayagrawal@gmail.com>
2025-11-15 06:27:22 +0530

    fix

--- src/pages/booksPage.jsx
+++ src/pages/booksPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+const API = import.meta.env.VITE_API_URL;
 
 function SelectChapter() {
   const navigate = useNavigate();
